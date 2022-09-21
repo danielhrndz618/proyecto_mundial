6d7abf68337e545615a5672bf394141f9176bcfa
Olivers11 <olstertecn597@gmail.com>
2022-09-21 15:58:43 -0600

    adding mundial pet and customize buttons

--- proyecto_mundial/Form1.Designer.cs
+++ proyecto_mundial/Form1.Designer.cs
@@ -32,7 +32,16 @@
             this.container_top = new System.Windows.Forms.Panel();
             this.container_left = new System.Windows.Forms.Panel();
             this.container_center = new System.Windows.Forms.Panel();
+            this.panel_margin = new System.Windows.Forms.Panel();
+            this.add_team = new System.Windows.Forms.Button();
+            this.add_player = new System.Windows.Forms.Button();
+            this.add_couch = new System.Windows.Forms.Button();
+            this.btn_game = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.container.SuspendLayout();
+            this.container_left.SuspendLayout();
+            this.panel_margin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // container
@@ -58,6 +67,11 @@
             // container_left
             // 
             this.container_left.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(0)))), ((int)(((byte)(7)))));
+            this.container_left.Controls.Add(this.btn_game);
+            this.container_left.Controls.Add(this.add_couch);
+            this.container_left.Controls.Add(this.add_player);
+            this.container_left.Controls.Add(this.add_team);
+            this.container_left.Controls.Add(this.panel_margin);
             this.container_left.Dock = System.Windows.Forms.DockStyle.Left;
             this.container_left.Location = new System.Drawing.Point(0, 26);
             this.container_left.Name = "container_left";
@@ -73,6 +87,102 @@
             this.container_center.Size = new System.Drawing.Size(792, 467);
             this.container_center.TabIndex = 2;
             // 
+            // panel_margin
+            // 
+            this.panel_margin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel_margin.Controls.Add(this.pictureBox1);
+            this.panel_margin.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_margin.Location = new System.Drawing.Point(0, 0);
+            this.panel_margin.Name = "panel_margin";
+            this.panel_margin.Size = new System.Drawing.Size(213, 122);
+            this.panel_margin.TabIndex = 0;
+            // 
+            // add_team
+            // 
+            this.add_team.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(0)))), ((int)(((byte)(7)))));
+            this.add_team.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.add_team.Dock = System.Windows.Forms.DockStyle.Top;
+            this.add_team.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(0)))), ((int)(((byte)(7)))));
+            this.add_team.FlatAppearance.BorderSize = 0;
+            this.add_team.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(118)))), ((int)(((byte)(40)))));
+            this.add_team.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(189)))), ((int)(((byte)(121)))));
+            this.add_team.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.add_team.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.add_team.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.add_team.Location = new System.Drawing.Point(0, 122);
+            this.add_team.Name = "add_team";
+            this.add_team.Size = new System.Drawing.Size(213, 39);
+            this.add_team.TabIndex = 1;
+            this.add_team.Text = "Agregar Equipo";
+            this.add_team.UseVisualStyleBackColor = false;
+            // 
+            // add_player
+            // 
+            this.add_player.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(0)))), ((int)(((byte)(7)))));
+            this.add_player.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.add_player.Dock = System.Windows.Forms.DockStyle.Top;
+            this.add_player.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(0)))), ((int)(((byte)(7)))));
+            this.add_player.FlatAppearance.BorderSize = 0;
+            this.add_player.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(118)))), ((int)(((byte)(40)))));
+            this.add_player.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(189)))), ((int)(((byte)(121)))));
+            this.add_player.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.add_player.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.add_player.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.add_player.Location = new System.Drawing.Point(0, 161);
+            this.add_player.Name = "add_player";
+            this.add_player.Size = new System.Drawing.Size(213, 39);
+            this.add_player.TabIndex = 2;
+            this.add_player.Text = "Agregar Jugador";
+            this.add_player.UseVisualStyleBackColor = false;
+            // 
+            // add_couch
+            // 
+            this.add_couch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(0)))), ((int)(((byte)(7)))));
+            this.add_couch.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.add_couch.Dock = System.Windows.Forms.DockStyle.Top;
+            this.add_couch.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(0)))), ((int)(((byte)(7)))));
+            this.add_couch.FlatAppearance.BorderSize = 0;
+            this.add_couch.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(118)))), ((int)(((byte)(40)))));
+            this.add_couch.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(189)))), ((int)(((byte)(121)))));
+            this.add_couch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.add_couch.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.add_couch.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.add_couch.Location = new System.Drawing.Point(0, 200);
+            this.add_couch.Name = "add_couch";
+            this.add_couch.Size = new System.Drawing.Size(213, 39);
+            this.add_couch.TabIndex = 3;
+            this.add_couch.Text = "Agregar Entrenador";
+            this.add_couch.UseVisualStyleBackColor = false;
+            // 
+            // btn_game
+            // 
+            this.btn_game.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(0)))), ((int)(((byte)(7)))));
+            this.btn_game.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_game.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_game.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(0)))), ((int)(((byte)(7)))));
+            this.btn_game.FlatAppearance.BorderSize = 0;
+            this.btn_game.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(118)))), ((int)(((byte)(40)))));
+            this.btn_game.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(189)))), ((int)(((byte)(121)))));
+            this.btn_game.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_game.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_game.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_game.Location = new System.Drawing.Point(0, 239);
+            this.btn_game.Name = "btn_game";
+            this.btn_game.Size = new System.Drawing.Size(213, 39);
+            this.btn_game.TabIndex = 4;
+            this.btn_game.Text = "Jugar Partido";
+            this.btn_game.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::proyecto_mundial.Properties.Resources.mascota1;
+            this.pictureBox1.Location = new System.Drawing.Point(26, -14);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 130);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -83,6 +193,9 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.container.ResumeLayout(false);
+            this.container_left.ResumeLayout(false);
+            this.panel_margin.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -93,6 +206,12 @@
         private System.Windows.Forms.Panel container_center;
         private System.Windows.Forms.Panel container_left;
         private System.Windows.Forms.Panel container_top;
+        private System.Windows.Forms.Panel panel_margin;
+        private System.Windows.Forms.Button add_team;
+        private System.Windows.Forms.Button add_couch;
+        private System.Windows.Forms.Button add_player;
+        private System.Windows.Forms.Button btn_game;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
