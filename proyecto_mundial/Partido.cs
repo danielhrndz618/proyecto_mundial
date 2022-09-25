2d84619c1d5bd6df7ad52fe27855f2e82e06cd24
Olivers11 <olstertecn597@gmail.com>
2022-09-24 22:57:09 -0600

    showing flags

--- proyecto_mundial/Partido.cs
+++ proyecto_mundial/Partido.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace proyecto_mundial
 {
@@ -15,6 +16,46 @@ namespace proyecto_mundial
         public Partido()
         {
             InitializeComponent();
+            this.fillData();
+        }
+
+        public void fillData()
+        {
+            TeamController tc = new TeamController();
+            var teams = tc.getTeams();
+            HelperGame hg = new HelperGame();
+            List<TeamModel> game_teams = hg.getGameTeams(teams);
+            team_1.Text = game_teams[0].name;
+            team_2.Text = game_teams[1].name;
+            this.ponerImagen(img_1, team_1.Text.ToLower());
+            this.ponerImagen(img_2, team_2.Text.ToLower());
+        }
+        public bool fileExist(string path, string filename)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            FileInfo[] files = directory.GetFiles();
+
+            foreach (FileInfo file in files)
+            {
+                if (String.Compare(file.Name, filename) == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void ponerImagen(PictureBox pic, String pais)
+        {
+            if(pais == "")
+            {
+                pic.Image.Dispose();
+                pic.Image = null;
+                return;
+            }
+            if (fileExist("./Banderas", pais + ".png"))
+            {
+                pic.ImageLocation = "./Banderas/" + pais + ".png";
+            }
         }
     }
 }
