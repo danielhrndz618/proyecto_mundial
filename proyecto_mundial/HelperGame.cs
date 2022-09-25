10d0d8f20963a4de04cb88d4ec443ea740a3ec7a
Olivers11 <olstertecn597@gmail.com>
2022-09-24 22:22:44 -0600

    adding function for get two teams for game

---
+++ proyecto_mundial/HelperGame.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto_mundial
+{
+    public class HelperGame
+    {
+        public Random random_generator;
+        public HelperGame()
+        {
+            this.random_generator = new Random();
+        }
+
+        int getRandPos(int size)
+        {
+            return random_generator.Next(0, size);
+        }
+
+
+
+
+        List<TeamModel> getGameTeams(List<TeamModel> teams)
+        {
+            int x = this.getRandPos(teams.Count);
+            int y = this.getRandPos(teams.Count);
+            List<TeamModel> game_teams = new List<TeamModel>();
+            game_teams.Add(teams[x]);
+            game_teams.Add(teams[y]);
+            return game_teams;
+        }
+
+
+
+
+
+    }
+}
